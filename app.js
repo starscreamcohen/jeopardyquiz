7baeae087c7cea1bcecedfe5def72ea771db2f21
Benjamin Cohen <benjamin.cohen86@gmail.comgit config --global user.emailgit config --global user.email benjamin.cohen@gmail.comgit config --global user.email benjamin.cohen86@gmail.com>
2014-08-07 23:33:03 -0400

    sets the correct answer

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ function categoryObj(category, id) {
 	this.id = id; 
 }
 
-var movie = new categoryObj("90's Movies", 0);
+var movie = new categoryObj("Movie Duos", 0);
 var capitals = new categoryObj("State Capitals", 1);
 
 function Question(clue, category_id, choices) {
 	this.clue = clue;
 	this.category_id = category_id;
 }
-	Question.prototype.choices = [];
+
+	
 
 var question = new Array();
 question[0] = new Question("What Comedy Duo Starred in Swingers?", movie);
 question[0].choices = {0: "Adam Sandler & Rob Schneider", 1: "Jon Favreau & Vince Vaughan", 2: "John Goodman & Tim Blake Nelson", 3: "Dave Chappelle & Kevin Harland"}
-
-
+question[0].correctAnswer = question[0].choices[0];
 question[1] = new Question("What's the Capital of Vermont", capitals);
 
 //model name, picture and update score
